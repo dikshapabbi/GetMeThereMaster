58fd64e15261c7c4487973af13b4d2c0809ccd03
dikshapabbi <dikshapabbi@ymail.com>
2017-06-12 23:40:43 +1200

    Get Me There

---
+++ app/src/main/java/com/GetMeThere/GetMeThereLogin/AlarmReceiver.java
@@ -0,0 +1,36 @@
+package com.GetMeThere.GetMeThereLogin;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by dpabb on 12/06/2017.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, ProfileFragment.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(ProfileFragment.class);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        Notification notification = builder.setContentTitle("Reminder")
+                .setContentText("You Have a Class starting in 10 mintues")
+                .setTicker("Alert!")
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+}
